51ef1a6ebb4a3d5ad246fb517c634ef7b59a0a90
Jonny Burger <jonathanburger11@gmail.com>
2019-05-29 16:21:57 +0200

    Move screenshots

--- stickers.framerfx/code/Screenshot.tsx
+++ stickers.framerfx/code/Screenshot.tsx
@@ -1,6 +1,23 @@
 import * as React from 'react';
-import {Frame, ControlType, addPropertyControls} from 'framer';
+import styled from 'styled-components';
+import {ControlType, addPropertyControls} from 'framer';
 import {url} from 'framer/resource';
+
+const Container = styled.div`
+	justify-content: center;
+	align-items: center;
+	display: flex;
+	background: linear-gradient(-45deg, rgb(88, 81, 219), rgb(64, 93, 230));
+	flex-direction: column;
+`;
+
+const Title = styled.div`
+	font-family: 'IBM Plex Sans';
+	color: white;
+	font-size: 2em;
+	font-weight: bold;
+	text-align: center;
+`;
 
 type Device = 'iphone-5-8-inch' | 'iphone-6-5-inch' | 'android';
 
@@ -17,17 +34,28 @@ export function Screenshot(props: Props) {
 			? url('./code/assets/iphone-8-plus.png')
 			: url('./code/assets/pixel3.png');
 	return (
-		<Frame
-			width={414}
-			height={height}
-			position="relative"
+		<Container
 			style={{
-				background:
-					'linear-gradient(-45deg, rgb(88, 81, 219), rgb(64, 93, 230))'
+				width: 414,
+				height
 			}}
 		>
-			<img src={image} style={{width: 300}} />
-		</Frame>
+			<div style={{marginTop: 100}}>
+				<Title>Create awesome stickers</Title>
+				{props.device === 'android' ? <div style={{height: 20}} /> : null}
+				{props.device === 'iphone-5-8-inch' ? (
+					<img src={image} style={{width: 400}} />
+				) : null}
+				{props.device === 'android' ? (
+					<img src={image} style={{width: 360}} />
+				) : null}
+				{props.device === 'iphone-6-5-inch' ? (
+					<div>
+						<img src={image} style={{width: 440}} />
+					</div>
+				) : null}
+			</div>
+		</Container>
 	);
 }
 
