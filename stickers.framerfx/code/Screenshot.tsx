a9f4c1c481607ba53f0e72468144ff61ff102e54
Jonny Burger <jonathanburger11@gmail.com>
2019-05-29 17:10:22 +0200

    component refined

--- stickers.framerfx/code/Screenshot.tsx
+++ stickers.framerfx/code/Screenshot.tsx
@@ -4,7 +4,6 @@ import {ControlType, addPropertyControls} from 'framer';
 import {url} from 'framer/resource';
 
 const Container = styled.div`
-	justify-content: center;
 	align-items: center;
 	display: flex;
 	background: linear-gradient(-45deg, rgb(88, 81, 219), rgb(64, 93, 230));
@@ -14,7 +13,7 @@ const Container = styled.div`
 const Title = styled.div`
 	font-family: 'IBM Plex Sans';
 	color: white;
-	font-size: 2em;
+	font-size: 2.2em;
 	font-weight: bold;
 	text-align: center;
 `;
@@ -23,10 +22,52 @@ type Device = 'iphone-5-8-inch' | 'iphone-6-5-inch' | 'android';
 
 type Props = {
 	device: Device;
+	screenshot: string;
+	slogan: string;
 };
 
-export function Screenshot(props: Props) {
-	const height = props.device === 'iphone-6-5-inch' ? 896 : 736;
+const getStyle = (device: Device): React.CSSProperties => {
+	if (device === 'iphone-5-8-inch') {
+		return {
+			width: 398,
+			left: -49,
+			top: -98,
+			position: 'absolute'
+		};
+	}
+	if (device === 'iphone-6-5-inch') {
+		return {
+			width: 435,
+			position: 'absolute',
+			left: -43,
+			top: -57
+		};
+	}
+	return {
+		width: 410,
+		position: 'absolute',
+		left: -29,
+		top: -77
+	};
+};
+
+const getScreenWidth = (device: Device): React.CSSProperties => {
+	if (device === 'iphone-6-5-inch') {
+		return {
+			width: 350
+		};
+	}
+	if (device === 'android') {
+		return {
+			width: 350
+		};
+	}
+	return {
+		width: 300
+	};
+};
+
+const Frame = (props: {device: Device; screen: string}) => {
 	const image =
 		props.device === 'iphone-6-5-inch'
 			? url('./code/assets/iphone-x.png')
@@ -34,26 +75,30 @@ export function Screenshot(props: Props) {
 			? url('./code/assets/iphone-8-plus.png')
 			: url('./code/assets/pixel3.png');
 	return (
+		<div style={{position: 'relative', marginTop: 100}}>
+			<img src={props.screen} style={getScreenWidth(props.device)} />
+			<img src={image} style={getStyle(props.device)} />
+		</div>
+	);
+};
+
+export function Screenshot(props: Props) {
+	const height = props.device === 'iphone-6-5-inch' ? 896 : 736;
+
+	return (
 		<Container
 			style={{
 				width: 414,
 				height
 			}}
 		>
-			<div style={{marginTop: 100}}>
-				<Title>Create awesome stickers</Title>
-				{props.device === 'android' ? <div style={{height: 20}} /> : null}
-				{props.device === 'iphone-5-8-inch' ? (
-					<img src={image} style={{width: 400}} />
-				) : null}
-				{props.device === 'android' ? (
-					<img src={image} style={{width: 360}} />
-				) : null}
+			<div style={{marginTop: 50}}>
+				<Title>{props.slogan}</Title>
+				{props.device === 'android' ? <div style={{marginTop: -10}} /> : null}
 				{props.device === 'iphone-6-5-inch' ? (
-					<div>
-						<img src={image} style={{width: 440}} />
-					</div>
+					<div style={{marginTop: -50}} />
 				) : null}
+				<Frame screen={props.screenshot} device={props.device} />
 			</div>
 		</Container>
 	);
@@ -66,11 +111,23 @@ addPropertyControls(Screenshot, {
 		defaultValue: 'iphone-5-8-inch',
 		options: ['iphone-5-8-inch', 'iphone-6-5-inch', 'android'],
 		optionTitles: ['iPhone 5.8 inch', 'iPhone 6.5 inch', 'Android']
+	},
+	screenshot: {
+		type: ControlType.Image,
+		title: 'Screenshot',
+		defaultValue: url('./code/screens/iPhone 1.PNG')
+	},
+	slogan: {
+		type: ControlType.String,
+		title: 'Slogan',
+		defaultValue: 'Create awesome stickers'
 	}
 });
 
 const defaultProps: Props = {
-	device: 'iphone-5-8-inch'
+	device: 'iphone-5-8-inch',
+	screenshot: url('./code/screens/iPhone 1.PNG'),
+	slogan: 'Create awesome stickers'
 };
 
 Screenshot.defaultProps = defaultProps;
